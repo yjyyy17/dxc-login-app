f7c5afabeebf84e1218c506c18c54b07fe8aa221
Jing Yi Yeo <jingyi.yeo.2020@scis.smu.edu.sg>
2024-03-12 11:18:43 +0800

    sql file for testing

---
+++ springboot-backend/sql/test.sql
@@ -0,0 +1,4 @@
+use `dxc_login`;
+INSERT INTO users (username, password, name, role) VALUES
+('dxc_user', 'dxc_user', 'user', 'ROLE_USER'),
+('dxc_manager', 'dxc_manager', 'manager', 'ROLE_MANAGER');
--- springboot-backend/src/main/java/com/dxc/springbootbackend/model/User.java
+++ springboot-backend/src/main/java/com/dxc/springbootbackend/model/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
